8e3c559d86c37ec73a2da28ebcf456ee81b1bf1a
gmgarin <96095956+gmgarin@users.noreply.github.com>
2022-04-03 20:47:50 -0400

    Mod charts.js file.

--- charts.js
+++ charts.js
@@ -93,7 +93,7 @@ d3.json("samples.json").then((data) => {
     type: "bar",
     orientation: 'h',
     marker: {
-      color: 'rgb(158,202,225)',
+      color: 'rgb(206, 186, 245)',
       opacity: 0.8,}
   }];
   
